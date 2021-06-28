3d56de7d01a048a3472197bdc7f8e5d719b79e01
EmersonPaduan <emerson.paduan@gmail.com>
2021-06-28 12:40:02 -0300

    exerc relogio alteração método setHora

--- aula4/exercicios/exercicio4/AppRelogio.java
+++ aula4/exercicios/exercicio4/AppRelogio.java
@@ -4,6 +4,13 @@ public class AppRelogio {
     public static void main(String[] args) {
         Relogio reg = new Relogio(16, 5, 6);
 
+
+        if(reg.setHora(-4)) {
+            System.out.println("Hora alterada");
+        } else {
+            System.out.println("Hora inválida. Não alterada.");
+        }
+
         reg.setMinuto(-12); // tentativa de atribuir valor inválido
 
         System.out.println("Hora: " + reg.getHora());
