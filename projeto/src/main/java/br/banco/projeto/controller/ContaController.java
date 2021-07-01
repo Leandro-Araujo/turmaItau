32ca7cb2d3faffb216cc9ed7ef4f5469290eb199
EmersonPaduan <emerson.paduan@gmail.com>
2021-07-01 15:04:15 -0300

    aula8-novaconta

--- projeto/src/main/java/br/banco/projeto/controller/ContaController.java
+++ projeto/src/main/java/br/banco/projeto/controller/ContaController.java
@@ -1,13 +1,27 @@
 package br.banco.projeto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.banco.projeto.model.Conta;
+import br.banco.projeto.services.ContaService;
 
 @RestController // indica que esta classe é um Controller REST
 @CrossOrigin("*") // aceita requisições de outros servidores
 @RequestMapping("/conta") // nomo usado no "recurso" da URL
 public class ContaController {
 
-    
+    @Autowired
+    private ContaService service;
+
+    @PostMapping("/nova")
+    public ResponseEntity<Conta> criarConta() {
+        Conta novaConta = service.criarNovaConta();
+        return ResponseEntity.ok(novaConta);
+    }
+
 }
