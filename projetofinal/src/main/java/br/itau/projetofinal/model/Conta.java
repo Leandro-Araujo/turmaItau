ca61177105cef2e176283506fca351bd48dfd307
EmersonPaduan <emerson.paduan@gmail.com>
2021-07-02 16:43:25 -0300

    projetoFinal-Services

--- projetofinal/src/main/java/br/itau/projetofinal/model/Conta.java
+++ projetofinal/src/main/java/br/itau/projetofinal/model/Conta.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,10 +17,59 @@ public class Conta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "numero")
     private long numero;
-    
+
+    @Column(name = "agencia")
     private int agencia;
+
+    @Column(name = "tipo")
     private int tipo;
+
+    @Column(name = "saldo")
     private double saldo;
+
+    @OneToOne // uma conta relacionada a 1 Cliente 
+    @JoinColumn(name = "cod_cliente") // código do cliente que é dono desta conta
     private Cliente titular;
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+    
     
 }
---
+++ projetofinal/src/main/java/br/itau/projetofinal/services/ClienteService.java
@@ -0,0 +1,6 @@
+package br.itau.projetofinal.services;
+
+
+public class ClienteService {
+    
+}
