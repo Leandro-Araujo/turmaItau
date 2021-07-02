b9007bded1a5ccfe8332e5ab2448bd487ff13a96
EmersonPaduan <emerson.paduan@gmail.com>
2021-07-02 16:43:04 -0300

    projetoFinal-ContaService

---
+++ projetofinal/src/main/java/br/itau/projetofinal/controller/ContaController.java
@@ -0,0 +1,12 @@
+package br.itau.projetofinal.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/contas")
+public class ContaController {
+    
+}
